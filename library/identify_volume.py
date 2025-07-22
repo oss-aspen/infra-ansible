8c6792fa6d7a9486bfbf265ac059950ce2aa7f3d
Adrian Edwards <adredwar@redhat.com>
2025-07-22 17:21:40 -0400

    light modifications

--- library/identify_volume.py
+++ library/identify_volume.py
@@ -75,8 +75,7 @@ from ansible.module_utils.basic import AnsibleModule
 def run_module():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
-        name=dict(type='str', required=True),
-        new=dict(type='bool', required=False, default=False)
+        mount_point=dict(type='str', required=True)
     )
 
     # seed the result dict in the object
@@ -96,14 +95,8 @@ def run_module():
     # supports check mode
     module = AnsibleModule(
         argument_spec=module_args,
-        supports_check_mode=True
+        supports_check_mode=False
     )
-
-    # if the user is working with this module in only check mode we do not
-    # want to make any changes to the environment, just return the current
-    # state with no modifications
-    if module.check_mode:
-        module.exit_json(**result)
 
     # manipulate or modify the state as needed (this is going to be the
     # part where your module will do what it needs to do)
@@ -113,13 +106,16 @@ def run_module():
     # use whatever logic you need to determine whether or not this module
     # made any modifications to your target
     if module.params['new']:
-        result['changed'] = True
+        result['changed'] = False
 
     # during the execution of the module, if there is an exception or a
     # conditional state that effectively causes a failure, run
     # AnsibleModule.fail_json() to pass in the message and the result
     if module.params['name'] == 'fail me':
         module.fail_json(msg='You requested this to fail', **result)
+
+    # nothing should ever change as a result of this module
+    result['changed'] = False
 
     # in the event of a successful module execution, you will want to
     # simple AnsibleModule.exit_json(), passing the key/value results
