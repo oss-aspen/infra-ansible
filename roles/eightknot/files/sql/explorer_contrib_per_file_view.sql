a7ad95148f6b1c82b21d4319580847a7f52a4c8b
Adrian Edwards <adredwar@redhat.com>
2026-01-27 09:15:41 -0500

    Fix views from caio
    Somehow, somewhere these got changed???

--- roles/eightknot/files/sql/explorer_contrib_per_file_view.sql
+++ roles/eightknot/files/sql/explorer_contrib_per_file_view.sql
@@ -1,16 +1,23 @@
 -- augur_data.explorer_cntrb_per_file source
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS augur_data.explorer_cntrb_per_file
-TABLESPACE pg_default
-AS SELECT pr.repo_id,
-    prf.pr_file_path AS file_path,
-    string_agg(DISTINCT pr.pr_augur_contributor_id::character varying(15)::text, ','::text) AS cntrb_ids,
-    string_agg(DISTINCT prr.cntrb_id::character varying(15)::text, ','::text) AS reviewer_ids
-   FROM augur_data.pull_requests pr
-     JOIN augur_data.pull_request_files prf ON pr.pull_request_id = prf.pull_request_id
-     LEFT JOIN augur_data.pull_request_reviews prr ON pr.pull_request_id = prr.pull_request_id
-  GROUP BY prf.pr_file_path, pr.repo_id
+CREATE MATERIALIZED VIEW IF NOT EXISTS augur_data.explorer_cntrb_per_file AS
+SELECT
+    pr.repo_id as repo_id,
+    prf.pr_file_path as file_path,
+    string_agg(DISTINCT CAST(pr.pr_augur_contributor_id AS varchar(15)), ',') AS cntrb_ids,
+    string_agg(DISTINCT CAST(prr.cntrb_id AS varchar(15)), ',') AS reviewer_ids
+FROM
+    augur_data.pull_requests pr
+INNER JOIN
+    augur_data.pull_request_files prf
+ON
+    pr.pull_request_id = prf.pull_request_id
+LEFT OUTER JOIN
+    augur_data.pull_request_reviews prr
+ON
+    pr.pull_request_id = prr.pull_request_id
+GROUP BY prf.pr_file_path, pr.repo_id
 WITH NO DATA;
 
--- View indexes:
+-- Create index for performance
 CREATE INDEX IF NOT EXISTS idx_explorer_cntrb_per_file_repo_id ON augur_data.explorer_cntrb_per_file USING btree (repo_id);
--- roles/eightknot/files/sql/explorer_pr_files_view.sql
+++ roles/eightknot/files/sql/explorer_pr_files_view.sql
@@ -1,13 +1,17 @@
--- augur_data.explorer_pr_files source
+-- Create explorer_pr_files materialized view
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS augur_data.explorer_pr_files
-TABLESPACE pg_default
-AS SELECT prf.pr_file_path AS file_path,
-    pr.pull_request_id,
-    pr.repo_id
-   FROM augur_data.pull_requests pr
-     JOIN augur_data.pull_request_files prf ON pr.pull_request_id = prf.pull_request_id
+CREATE MATERIALIZED VIEW IF NOT EXISTS augur_data.explorer_pr_files AS
+SELECT
+    prf.pr_file_path as file_path,
+    pr.pull_request_id AS pull_request_id,
+    pr.repo_id as repo_id
+FROM
+    augur_data.pull_requests pr
+INNER JOIN
+    augur_data.pull_request_files prf
+ON
+    pr.pull_request_id = prf.pull_request_id
 WITH NO DATA;
 
--- View indexes:
-CREATE INDEX IF NOT EXISTS idx_explorer_pr_files_repo_id ON augur_data.explorer_pr_files USING btree (repo_id);
+-- Create index for performance
+CREATE INDEX idx_explorer_pr_files_repo_id ON augur_data.explorer_pr_files(repo_id);
